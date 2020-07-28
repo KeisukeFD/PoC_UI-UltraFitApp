5b0c35ba06a51d747034b68eb311abd7f37ab8ff
Valentin Malissen <keisuke.tk.fd@gmail.com>
2020-07-28 10:54:34 -0400

    Initial Commit

---
+++ UI-UtraFitApp/UI_UtraFitAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  UI_UtraFitAppApp.swift
+//  UI-UtraFitApp
+//
+//  Created by Valentin Malissen on 2020-07-28.
+//
+
+import SwiftUI
+
+@main
+struct UI_UtraFitAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
